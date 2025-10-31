5e6aa6a099baca76aadb82af588c310763414c30
fryerd1 <dylan.fryer@ons.gov.uk>
2025-10-31 10:01:01 +0000

    Change health_data csv to have missing values, change src folder name to R

--- R/R_rap_demo/cleaning.R
+++ R/R_rap_demo/cleaning.R
@@ -10,9 +10,8 @@ clean_health_data <- function(df) {
   #Returns:
   #    df_clean (DataFrame): Cleaned health data DataFrame.
   
-  df_clean <- df
   # Drop rows with missing diagnosis
-  df_clean <- df_clean[complete.cases(df_clean["diagnosis"]),]
+  df_clean <- df[!is.na(df$diagnosis),]
   
   # Fill missing smoker values with 'No'
   df_clean$smoker[is.na(df_clean$smoker)] <- "No"
