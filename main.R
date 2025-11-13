03cefba65b8487be8d23bdb734f0e9dcdec995b6
Alex Westwood <alex.westwood@ons.gov.uk>
2025-11-13 13:51:11 +0000

    apply style changes

--- main.R
+++ main.R
@@ -15,21 +15,21 @@ main <- function() {
   # Load the YAML configuration file
   config <- yaml::read_yaml(config_path)
 
-  input_path = config$input_path
-  cleaned_path = config$cleaned_path
-  report_path = config$report_path
-  
+  input_path <- config$input_path
+  cleaned_path <- config$cleaned_path
+  report_path <- config$report_path
+
   # I/O Read data
   df <- read_health_data(input_path)
-  
+
   # Cleaning
   df_clean <- clean_health_data(df)
   df_clean <- add_bmi_column(df_clean)
   write_dataframe(df_clean, cleaned_path)
-  
+
   # Processing
-  prevalence_df = calculate_disease_prevalence(df_clean)
-  
+  prevalence_df <- calculate_disease_prevalence(df_clean)
+
   # Reporting
   generate_markdown_report(prevalence_df, report_path)
   cat("Report generated: ", report_path)
